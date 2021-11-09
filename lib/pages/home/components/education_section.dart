2fe96a263b411a17df83d0d73fbf0263d413a10c
Albenga96 <albertoferroni.af@gmail.com>
2021-11-09 17:21:25 +0100

    Updated educational

--- lib/pages/home/components/education_section.dart
+++ lib/pages/home/components/education_section.dart
@@ -10,25 +10,25 @@ final List<Education> educationList = [
   Education(
     description:
         "This is a sample education and details about it is stated below. This is a sample education and details about it is stated below",
-    linkName: "www.flutterpanda.com",
+    linkName: "www.albertoferroni.com",
     period: "2019 - PRESENT",
   ),
   Education(
     description:
         "This is a sample education and details about it is stated below.This is a sample education and details about it is stated below",
-    linkName: "www.flutterpanda.com",
+    linkName: "www.albertoferroni.com",
     period: "2018 - 2019",
   ),
   Education(
     description:
         "This is a sample education and details about it is stated below. This is a sample education and details about it is stated below",
-    linkName: "www.flutterpanda.com",
+    linkName: "www.albertoferroni.com",
     period: "2017 - 2018",
   ),
   Education(
     description:
         "This is a sample education and details about it is stated below. This is a sample education and details about it is stated below",
-    linkName: "www.flutterpanda.com",
+    linkName: "www.albertoferroni.com",
     period: "2016 - 2017",
   ),
 ];
